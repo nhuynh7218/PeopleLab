ef2838ebdf5b8ead9e2c14a42e80664929dfb7b8
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-22 09:29:17 -0500

    asd

--- Lab 2.2 People/src/Classroom.java
+++ Lab 2.2 People/src/Classroom.java
@@ -1,8 +1,33 @@
+import java.util.Arrays;
 
 public class Classroom {
+	private Person[] students;
+	private Person teacher;
+	
+	public Classroom(Person teacher, Person[] students) {
+		this.students=students;
+		this.teacher=teacher;
+	}
 
-	public Classroom() {
-		// TODO Auto-generated constructor stub
+	public Person[] getStudents() {
+		return students;
+	}
+
+	public void setStudents(Person[] students) {
+		this.students = students;
+	}
+
+	public Person getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Person teacher) {
+		this.teacher = teacher;
+	}
+
+	@Override
+	public String toString() {
+		return "Classroom [students=" + Arrays.getgPA + ", teacher=" + teacher + "]";
 	}
 
 }
